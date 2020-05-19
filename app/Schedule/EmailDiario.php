2e6208bedb33af36263ceee156fda1920ec596af
b2smarketing <valente@b2smarketing.com>
2020-05-19 17:15:43 -0300

    19-05-2020_17:15:42

--- app/Schedule/EmailDiario.php
+++ app/Schedule/EmailDiario.php
@@ -168,4 +168,4 @@ $schedule->call(function () {
             */
         }
     });
-})->timezone('America/Sao_Paulo')->dailyAt('17:08'); // Rodamos esse relatório todos os dias, às 10:37 da manhã
+})->timezone('America/Sao_Paulo')->dailyAt('17:17'); // Rodamos esse relatório todos os dias, às 10:37 da manhã
