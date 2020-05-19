cfb62bf62e93188e80f4a4d6122e4df0c8da814d
b2smarketing <valente@b2smarketing.com>
2020-05-19 16:50:54 -0300

    19-05-2020_16:50:54

--- app/Schedule/EmailDiario.php
+++ app/Schedule/EmailDiario.php
@@ -31,106 +31,113 @@ $schedule->call(function () {
         };
 
         // Aqui fazemos o loop por todas campanhas
-
+        $total_campanhas = 0;
+        $campanhas = [];
         foreach (Campanha::cursor() as $campanha) {
+            // Valida se campanha está ativa           
+            if ($campanha->relatorios == 1) {
+                $total_campanhas++;
+                array_push($campanhas,$campanha);
+            }
+        }
 
-            // Valida se campanha está ativa
-            if ($campanha->relatorios) {
+        for ($i=0;$i<$total_campanhas;$i++) {
+            $campanha = $campanhas[$i];
+            
+            _('Campanha: ' . $campanha->nome);
 
-                _('Campanha: ' . $campanha->nome);
+            // Leads
 
-                // Leads
+            $leads = $campanha->leads_total()->with('curso')->get();
+            $leads_hoje = $leads->filter(function ($lead) use ($exibir) {
+                return $exibir($lead->created_at);
+            });
 
-                $leads = $campanha->leads_total()->with('curso')->get();
-                $leads_hoje = $leads->filter(function ($lead) use ($exibir) {
-                    return $exibir($lead->created_at);
-                });
+            // Ignorar e-mails em branco
+            if ($leads->count() == 0)
+                continue;
 
-                // Ignorar e-mails em branco
-                if ($leads->count() == 0)
-                    continue;
+            // Inscritos
 
-                // Inscritos
+            // $inscritos = $campanha->inscritos_total()->with('curso')->get();
+            $inscritos = $campanha->inscritos_total_unique()->with('curso')->get();
+            $inscritos_hoje = $inscritos->filter(function ($lead) use ($exibir) {
+                return $exibir($lead->created_at);
+            });
 
-                // $inscritos = $campanha->inscritos_total()->with('curso')->get();
-                $inscritos = $campanha->inscritos_total_unique()->with('curso')->get();
-                $inscritos_hoje = $inscritos->filter(function ($lead) use ($exibir) {
-                    return $exibir($lead->created_at);
-                });
+            // Matriculados
 
-                // Matriculados
+            // $matriculados = $campanha->matriculados()->with('curso')->get();
+            $matriculados = $campanha->matriculados_total_unique()->with('curso')->get();
+            $matriculados_hoje = $matriculados->filter(function ($lead) use ($exibir) {
+                return $exibir($lead->created_at);
+            });
 
-                // $matriculados = $campanha->matriculados()->with('curso')->get();
-                $matriculados = $campanha->matriculados_total_unique()->with('curso')->get();
-                $matriculados_hoje = $matriculados->filter(function ($lead) use ($exibir) {
-                    return $exibir($lead->created_at);
-                });
+            // Cursos
 
-                // Cursos
+            $cursos = [];
 
-                $cursos = [];
+            foreach ($campanha->cursos()->cursor() as $curso) {
+                $c = [
+                    'curso' => $curso,
+                    'leads' => $leads_hoje->where('curso.id', $curso->id)->count(),
+                    'inscritos' => $inscritos_hoje->where('curso.id', $curso->id)->count(),
+                    'inscritos_total' => $inscritos->where('curso.id', $curso->id)->count(),
+                    'matriculados' => $matriculados_hoje->where('curso.id', $curso->id)->count(),
+                    'matriculados_total' => $matriculados->where('curso.id', $curso->id)->count()
+                ];
 
-                foreach ($campanha->cursos()->cursor() as $curso) {
-                    $c = [
-                        'curso' => $curso,
-                        'leads' => $leads_hoje->where('curso.id', $curso->id)->count(),
-                        'inscritos' => $inscritos_hoje->where('curso.id', $curso->id)->count(),
-                        'inscritos_total' => $inscritos->where('curso.id', $curso->id)->count(),
-                        'matriculados' => $matriculados_hoje->where('curso.id', $curso->id)->count(),
-                        'matriculados_total' => $matriculados->where('curso.id', $curso->id)->count()
-                    ];
+                $cursos[] = $c;
+            }
 
-                    $cursos[] = $c;
-                }
+            $cursos = collect($cursos)->sortBy('curso.nome');
 
-                $cursos = collect($cursos)->sortBy('curso.nome');
+            // Criar e-mail
+
+            _('Processando dados...');
+
+            $assunto = 'Relatório de Campanha: ' . $min->format('d/m/Y') . ' [' . $campanha->nome . ']';
+
+            $mail_body = view('AmbienteConversao::email-diario', [
+                'leads' => $leads,
+                'leads_hoje' => $leads_hoje,
+                'inscritos' => $inscritos,
+                'inscritos_hoje' => $inscritos_hoje,
+                'matriculados' => $matriculados,
+                'matriculados_hoje' => $matriculados_hoje,
+                'cursos' => $cursos
+            ])->render();
+
+            // Novo código para disparo de e-mail individual
+
+            _('Preparando disparos de e-mail...');
+
+            foreach (Datalist::on('email-diario')->cursor() as $dest) {
+
+                _('Disparando e-mail: ' . $dest->value);
 
                 // Criar e-mail
+                $email = Email::create($assunto)
+                    ->smtp_auth()
+                    ->from(env('SMTP_USER'), 'App FAM')
+                    ->to('everton.messias@gmail.com', 'Everton')
+                    //->to('eryvelton@fam.br','Eryvelton')
+                    //->to('gabriela@fam.br','Gabriela')
+                    ->html($mail_body);
 
-                _('Processando dados...');
-
-                $assunto = 'Relatório de Campanha: ' . $min->format('d/m/Y') . ' [' . $campanha->nome . ']';
-
-                $mail_body = view('AmbienteConversao::email-diario', [
-                    'leads' => $leads,
-                    'leads_hoje' => $leads_hoje,
-                    'inscritos' => $inscritos,
-                    'inscritos_hoje' => $inscritos_hoje,
-                    'matriculados' => $matriculados,
-                    'matriculados_hoje' => $matriculados_hoje,
-                    'cursos' => $cursos
-                ])->render();
-
-                // Novo código para disparo de e-mail individual
-
-                _('Preparando disparos de e-mail...');
-
-                foreach (Datalist::on('email-diario')->cursor() as $dest) {
-
-                    _('Disparando e-mail: ' . $dest->value);
-
-                    // Criar e-mail
-                    $email = Email::create($assunto)
-                        ->smtp_auth()
-                        ->from(env('SMTP_USER'), 'App FAM')
-                        ->to('everton.messias@gmail.com', 'Everton')
-                        //->to('eryvelton@fam.br','Eryvelton')
-                        //->to('gabriela@fam.br','Gabriela')
-                        ->html($mail_body);
-
-                    // Realizar disparo
-                    if ($email->send())
-                        _('Enviado com sucesso!');
-                    else {
-                        _('Falha ao enviar e-mail:');
-                        _($email->error());
-                    }
-
-                    // Deixar o servidor de e-mail "respirar" um pouco
-                    sleep(6);
+                // Realizar disparo
+                if ($email->send())
+                    _('Enviado com sucesso!');
+                else {
+                    _('Falha ao enviar e-mail:');
+                    _($email->error());
                 }
 
-                /*// Criar e-mail
+                // Deixar o servidor de e-mail "respirar" um pouco
+                sleep(6);
+            }
+
+            /*// Criar e-mail
             $email = Email::create($assunto)
                 ->smtp_auth()
                 ->from(env('SMTP_USER'), 'App FAM')
@@ -154,7 +161,6 @@ $schedule->call(function () {
                 _ ($email->error());
             }
             */
-            }
         }
     });
 })->timezone('America/Sao_Paulo')->dailyAt('16:30'); // Rodamos esse relatório todos os dias, às 10:37 da manhã
