a9c7040feb43f01564d73575fd3b95b138652748
b2smarketing <valente@b2smarketing.com>
2020-06-03 20:29:48 -0300

    03-06-2020_20:29:48

--- ambiente_conversao/vestibularfam2020.1/assets/js/update.js
+++ ambiente_conversao/vestibularfam2020.1/assets/js/update.js
@@ -7,16 +7,29 @@ function tipoprova() {
         $(".enem").css({"display":"none"})
         $(".formado").css({"display":"none"})
         $(".vestibular").slideDown(500);
+        $(".confirmaemail").css({"display":"none"})
     }
     else if (prova == "sim") {
         $(".formado").css({"display":"none"})
         $(".vestibular").css({"display":"none"})
         $(".enem").slideDown(500);
+        $(".confirmaemail").slideDown(500);
     }
     else if (prova == "segunda_graduacao") {
         $(".vestibular").css({"display":"none"})
         $(".enem").css({"display":"none"})
         $(".formado").slideDown(500);
+        $(".confirmaemail").slideDown(500);
     }
     
+}
+
+function tipodeficiencia() {
+    var deficiencia = $("#deficiencia").val();
+    if (deficiencia == "sim") {
+        $(".deficiencia").slideDown(500);
+    }
+    else if(deficiencia == "nao") {
+        $(".deficiencia").css({"display":"none"})
+    }    
 }
