ce1aa3a35d36b3e004793df77ca41ca7bc2cf9b0
evertonmessias <everton.messias@gmail.com>
2021-01-11 15:21:24 -0300

    11-01-2021_15:21:24

--- app/Modules/ACFAM2/routes/web.php
+++ app/Modules/ACFAM2/routes/web.php
@@ -819,21 +819,10 @@ Route::group(['middleware' => [$middle_dados]], function () use ($module) {
 		$mensagem = '
 		<html>
 		<head>
-		<title>Birthday Reminders for August</title>
+		<title>Teste</title>
 		</head>
 		<body>
-		<p>Here are the birthdays upcoming in August!</p>
-		<table>
-			<tr>
-			<th>Person</th><th>Day</th><th>Month</th><th>Year</th>
-			</tr>
-			<tr>
-			<td>Johny</td><td>10th</td><td>August</td><td>1970</td>
-			</tr>
-			<tr>
-			<td>Sally</td><td>17th</td><td>August</td><td>1973</td>
-			</tr>
-		</table>
+		<h1>Oii Amanda esse é um teste kkkkkkk!</h1>		
 		</body>
 		</html>
 		';
